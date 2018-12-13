5f1619a9dee2083279026bd1d9c890a90e04705c
VitaminOh <dlin01@stuy.edu>
2018-12-13 11:02:05 -0500

    finished Node class and started on MyLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,28 +1,50 @@
 class MyLinkedList{
- private int size;
- private Node start, end;
- public MyLinkedList(){
- }
- public int size(){
-   return this.size;
- }
- public boolean add(int value){
-   Node add = new node(value, end, null);
- }
- public String toString(){
-   String ans = start.data;
-   for(int i = 0; i < this.size; i++){
-     ans += start.next.data;
-   }
- }
-}
-
-class Node{
- private int data;
- private Node next, prev;
- public Node(int element, Node prevNode, Node nextNode){
-      this.data = element;
-      this.prev = prevNode;
-      this.next = nextNode;
+  private class Node{
+    private Integer data;
+    private Node next,prev;
+    public Node(Integer element, Node prevNode, Node nextNode){
+      data = element;
+      prev = prevNode;
+      next = nextNode;
     }
+    public Node next(){
+      return next;
+    }
+    public Node prev(){
+      return prev;
+    }
+    public void setNext(Node other){
+      next = other;
+    }
+    public void setPrev(Node other){
+      prev = other;
+    }
+    public Integer getData(){
+      return data;
+    }
+    public Integer setData(Integer value){
+      data = value;
+    }
+    public String toString(){
+      return data + "";
+    }
+  }
+  private int length;
+  private Node start,end;
+  public MyLinkedList(){
+    Node startNode = new Node(null,null,null);
+    Node endNode = new Node(null,null,null);
+  }
+  public int size(){
+    return this.length;
+  }
+  public boolean add(int value){
+    Node add = new node(value, end, null);
+  }
+  public String toString(){
+    String ans = start.data;
+    for(int i = 0; i < this.length; i++){
+      ans += start.next.data;
+    }
+  }
 }
