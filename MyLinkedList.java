5c5ed72e5e3ea9792c68446dddcbb164a044e5e1
VitaminOh <dlin01@stuy.edu>
2018-12-14 10:50:15 -0500

    added size and toString methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -32,14 +32,34 @@ class MyLinkedList{
   private int length;
   private Node start,end;
   public MyLinkedList(){
-    Node startNode = new Node(null,null,null);
-    Node endNode = new Node(null,null,null);
-    startNode.setNext(endNode);
-    endNode.setPrev(startNode);
+    Node start = new Node(null,null,null);
+    Node end = new Node(null,null,null);
+    start.setNext(end);
+    end.setPrev(start);
+    length = 0;
   }
   public boolean add(Integer value){
-    Node addToEnd = new Node(value, endNode, null);
-    endNode.setNext(addToEnd);
-    return true;
+    if (length == 0){
+      start.setData(value);
+      length++;
+      return true;
+    } else {
+      Node addToEnd = new Node(value,end,null);
+      end.setNext(addToEnd);
+      end = addToEnd;
+      length++;
+      return true;
+    }
+  }
+  public int size(){
+    return length;
+  }
+  public String toString(){
+    current = start;
+    String ans = "";
+    while(current.next != null){
+      and += current.getData();
+      current = current.next;
+    }
   }
 }
